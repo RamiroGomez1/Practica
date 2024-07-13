3fcc739c00062f534aaa0d7db84cef2d0c617f02
Ramiro Gomez <ramigomezbahia@gmail.com>
2024-07-12 23:23:54 -0300

    Arregle los botones y las distancias y tamaños

--- practica/50days50projects/P3/tailwind.config.js
+++ practica/50days50projects/P3/tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
+    fontFamily: {
+    'Overpass': ['Overpass']
+  },
     extend: {},
+    
   },
   plugins: [],
 }
