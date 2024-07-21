36c0f162f1aea2873fdab32ff3258d83051f88ff
Ramiro Gomez <ramigomezbahia@gmail.com>
2024-07-21 13:40:47 -0300

    Corregi el error de cantidades de cada producto

--- practica/50days50projects/P4/app.js
+++ practica/50days50projects/P4/app.js
@@ -2,14 +2,11 @@
 document.addEventListener('DOMContentLoaded',()=>{
     
     const addCartButton = document.querySelectorAll(".button");
-    const cartCake = document.getElementById('cartCake');
     const cartItems = document.getElementById('cart');
     const nothingCart = document.getElementById('nothingCart')
     const addedItemsText = document.getElementById('youAddedItems')
     let cartProducts = [];
-    
-    let productId;
-    
+        
     addCartButton.forEach(button => {
         button.addEventListener('click',()=>{
             
@@ -21,57 +18,89 @@ document.addEventListener('DOMContentLoaded',()=>{
 
             const type = productElement.querySelector('#type').textContent.trim()
             const product = productElement.querySelector('#product').textContent.trim()
-            const price = productElement.querySelector('#price').textContent.trim()
+            const price = productElement.querySelector('#price').textContent.trim()            
+            const productId = button.getAttribute('id');
+
+            const existingProduct = cartProducts.find(item => item.id === productId);
             
+
+            if(existingProduct){existingProduct.amount += 1;
+
+            }else {
+                const productDetails = {
+                    id: productId,
+                    type: type,
+                    product: product,
+                    price: price,
+                    amount: 1
+                };
+                cartProducts.push(productDetails);
+            }
             
-            productId = button.getAttribute('id');
-            
-            const productDetails = {
-                id: productId,
-                type: type,
-                product: product,
-                price: price
-            };
-            
-            cartProducts.push(productDetails);
-            console.log(cartProducts);
+
 
             showProductInCart();
+
         })
     })
     
 
 
+    
+    
+    
+    
+    
+    
+    
     function showProductInCart() {
+        
+        cartItems.innerHTML = '';
+        
+        
+        if(cartProducts > 0) {
+            nothingCart.classList.add('hidden')
+        } else {
+            nothingCart.classList.remove('hidden')
+        };
+        
+        
+        
+        cartProducts.forEach(product => { 
 
+            const totalCost = (parseFloat(product.price.replace('$', '')) * product.amount).toFixed(2);
+
+            const newCartItem = document.createElement('div');
+            newCartItem.innerHTML = `
+            <div class="grid grid-rows-2 mt-5 border-b-2 p-1 text-nowrap">
+            <div>
+            <h1 class="mb-1 text-lg font-semibold">${product.type}</h1>
+            </div>
+            <div class="flex text-nowrap text-base items-center">
+            <h1 class="mx-2 text-orange-600 font-bold" id="amountOfProducts">x${product.amount}</h1>
+            <h1 class="mx-2 font-thin" id="costPerUnit">${product.price}</h1>
+            <h1 class="mx-2 font-bold" id="totalCostPerProduct">${totalCost}</h1>
+            <button class="ml-auto text-red-600 font-bold remove-btn" data-id="${product.id}">X</button>
+            </div>
+            </div>
+            `
+            cartItems.appendChild(newCartItem)
+            
+        })
+        addedItemsText.textContent = `You have added ${cartProducts.length} items to your cart.`;
         
 
-            if(cartProducts > 0) {
-                nothingCart.classList.add('hidden')
-            } else {
-                nothingCart.classList.remove('hidden')
-            };
-
-            cartProducts.forEach(product => { 
-
-                const newCartItem = document.createElement('div');
-                newCartItem.innerHTML = `
-                <div class="grid grid-rows-2 mt-5 border-b-2 p-1  text-nowrap">
-                <div>
-                <h1 class="mb-1 text-lg font-semibold">${product.type}</h1>
-                </div>
-                <div class="flex text-nowrap text-base items-center">
-                <h1 class="mx-2 font-thin" id="costPerUnit">${product.price}</h1>
-                <h1 class="mx-2 font-bold" id="totalCostPerProduct">${product.price}</h1>
-                <button class="ml-auto text-red-600 font-bold">X</button>
-                </div>
-                </div>
-                `
-                })
+        /* Falta darle funcionalidad de eliminar a los botones X y de poder remover de a 1 los items */
+        
+        const removeButtons =document.querySelectorAll('.remove-btn'); 
+        removeButtons.forEach(button => {
+            button.addEventListener('click', ()=> {
                 
-        cartItems.appendChild(newCartItem)
+            })
+        })
     }
-    addedItemsText.textContent = `You have added ${cartProducts.length} items to your cart.`;
-  
+
+
+   
 })
 
