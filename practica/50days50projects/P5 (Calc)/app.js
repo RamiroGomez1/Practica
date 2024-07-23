f1db18f231b362f9052e598ca12668af104f6e16
Ramiro Gomez <ramigomezbahia@gmail.com>
2024-07-23 18:20:10 -0300

    Comienzo proyecto Calculadora. Termine el Diseño. Empece funcionalidades botones

---
+++ practica/50days50projects/P5 (Calc)/app.js
@@ -0,0 +1,106 @@
+const Reset = document.getElementById('RESET');
+const equal = document.getElementById('equal');
+const x = document.getElementById('x');
+const / = document.getElementById('/');
+const 0 = document.getElementById('0');
+const punto = document.getElementById('.');
+const - = document.getElementById('-');
+const 3 = document.getElementById('3');
+const 2 = document.getElementById('2');
+const 1 = document.getElementById('1');
+const + = document.getElementById('+');
+const 6 = document.getElementById('6');
+const 5 = document.getElementById('5');
+const 4 = document.getElementById('4');
+const DEL = document.getElementById('DEL');
+const 9 = document.getElementById('9');
+const 8 = document.getElementById('8');
+const 7 = document.getElementById('7');
+
+Reset.addEventListener('click',()=>{
+
+});
+
+equal.addEventListener('click',()=>{
+
+});
+
+x.addEventListener('click',()=>{
+
+});
+
+/.addEventListener('click',()=>{
+
+});
+
+
+0.addEventListener('click',()=>{
+
+});
+
+
+punto.addEventListener('click',()=>{
+
+});
+
+
+-.addEventListener('click',()=>{
+
+});
+
+
+3.addEventListener('click',()=>{
+
+});
+
+
+2.addEventListener('click',()=>{
+
+});
+
+
+1.addEventListener('click',()=>{
+
+});
+
+
++.addEventListener('click',()=>{
+
+});
+
+
+6.addEventListener('click',()=>{
+
+});
+
+
+5.addEventListener('click',()=>{
+
+});
+
+
+4.addEventListener('click',()=>{
+
+});
+
+
+DEL.addEventListener('click',()=>{
+
+});
+
+
+9.addEventListener('click',()=>{
+
+});
+
+
+8.addEventListener('click',()=>{
+
+});
+
+
+7.addEventListener('click',()=>{
+
+});
+
+
