27471619ecadcec65353c6b611526e2dd8e32088
Ramiro Gomez <ramigomezbahia@gmail.com>
2024-07-26 18:39:35 -0300

    Practica syntaxis, efectos, selectores

---
+++ practica/jquery/js/script.js
@@ -0,0 +1,17 @@
+
+$( () => {
+
+    $('#fadeToggle').click(()=>{
+        $('#div1, #div2, #div3').fadeToggle(2000)
+    })
+
+    $('#stop').click(()=>{
+        $('#div3').stop()
+    })
+})
+
+
+
+// $
+// selectos
+// action
