f0d9b8f0e2927f6a3d4ba44070b513b25922d33d
Ramiro Gomez <ramigomezbahia@gmail.com>
2024-07-27 21:27:49 -0300

    Efectos jQuery

---
+++ practica/jquery/Efectos/js/script.js
@@ -0,0 +1,51 @@
+
+$( () => {
+
+let $div1 = $('#div1')
+
+    /* Fade
+    $('#fadeToggle').click(()=>{
+        $($div1, '#div2, #div3').fadeToggle(2000)
+    })
+
+    $('#stop').click(()=>{
+        $('#div3').stop()
+    })
+    */
+
+    /*  Slide
+    $('#slideup').click(()=>{
+        $('#div1, #div2, #div3').slideUp(2000)
+    })
+    
+    $('#slidedown').click(()=>{
+        $('#div1, #div2, #div3').slideDown(2000)
+    })
+    
+    $('#stop').click(()=>{
+        $('#div3').stop()
+    })
+    */
+
+    /* Animation */
+
+    $('#animate').click(()=>{
+        $div1.animate({
+            width:'toggle',
+            height:'toggle',
+        })
+    })
+
+    
+
+})
+
+
+
+
+
+
+
+// $
+// selectos
+// action
