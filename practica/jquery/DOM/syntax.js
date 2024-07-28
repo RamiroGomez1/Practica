0b60fc83e6cfbca434e8d5f8c9dfb8a4962ff829
Ramiro Gomez <ramigomezbahia@gmail.com>
2024-07-27 22:19:11 -0300

    Practica metodos (append, preappend, before, after ,attr ,text)

---
+++ practica/jquery/DOM/syntax.js
@@ -0,0 +1,56 @@
+$( () => {
+
+    /*
+    $("#dom").click(()=>{
+        alert($('#p1').text())
+        alert($('#p2').text())
+        alert($('#val').text())
+    })
+
+    $("#domModificado").click(()=>{
+        $('#p1').text('Contenido del parrafo 1')
+        $('#p2').text('Contenido del parrafo 2')
+        $('#val').text('Contenido del input')
+        alert($('#p1').text())
+        alert($('#p2').text())
+        alert($('#val').text())
+ 
+    })
+
+    $("#modificarEnlace").click(()=>{
+        $('#enlace').attr({
+            href: 'https://youtube.com',
+            title: 'youtube',
+            })
+            })
+            */
+           
+           /*  Metodos
+$("#dom").click(()=>{
+    $('p').append('Metodo append')
+})
+
+$("#dom2").click(()=>{
+    $('ol').append('<li>Metodo append</li>')
+})
+
+$("#dom3").click(()=>{
+    $('p').prepend('Metodo append')
+})
+
+$("#dom4").click(()=>{
+    $('ol').prepend('<li>Metodo append</li>')
+})
+
+$("#dom5").click(()=>{
+    $('p').after('Metodo append')
+})
+
+$("#dom6").click(()=>{
+    $('p').after('<li>Metodo append</li>')
+})
+
+*/
+
+
+})
