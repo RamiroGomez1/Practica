1a7c160cf12255ef9096a81a229ade6e59d50846
Ramiro Gomez <ramigomezbahia@gmail.com>
2024-08-06 20:51:44 -0300

    React DOCS: Re-renderizado de la pagina

---
+++ practica/React/React Docs/part3/src/main.jsx
@@ -0,0 +1,25 @@
+import ReactDOM from 'react-dom/client'
+
+import App from './App'
+
+let counter = 1
+
+//Podemos hacer que el componente 
+//se vuelva a renderizar llamando al método ReactDOM.render por segunda vez
+
+const refresh = () => {
+    ReactDOM.createRoot(document.getElementById('root')).render(
+        <App counter={counter} />
+
+        // El comando de re-renderizado se ha envuelto dentro de la 
+        // función refresh para reducir la cantidad de código copiado y pegado.
+
+    )
+}
+
+setInterval(()=>{
+    refresh()
+    counter += 1
+},1000)
+
+//Llamar en repetidas ocasiones no es una buena practica al momento de re-renderizar componentes, para eso esta el capitulo 'Componente con estado'
