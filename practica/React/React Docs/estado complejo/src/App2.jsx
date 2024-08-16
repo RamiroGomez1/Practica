2e9d1eb6cba4773eed275bad995ed193001ce875
Ramiro Gomez <ramigomezbahia@gmail.com>
2024-08-16 11:44:15 -0300

    Actualizacion del estado asincronica

---
+++ practica/React/React Docs/estado complejo/src/App2.jsx
@@ -0,0 +1,67 @@
+import { useState } from 'react'
+
+const App = () => {
+
+
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const [total, setTotal] = useState(0)
+
+
+  const handleLeftClick1 = () => {
+    setAll(allClicks.concat('L'))
+    console.log('left before', left)
+    setLeft(left + 1)
+    console.log('left after', left)
+    setTotal(left + right)
+  }
+
+  /*
+  Al ver la consola veo que se logea:
+
+  left before 0
+  left after 0
+  left before 1
+  left after 1
+
+  Esto se debe a que la renderizacion nueva de React ocurre aincronicamente
+  Esto significa que ocurre en algun momento antes de renderizarlo, no inmediatamente antes
+  Por lo tanto, necesitamos asegurar el renderizado del contador actualizado
+  Para esto creamos una nueva variable updatedLeft antes de renderizarlo
+  */
+
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedRight = right + 1
+    setLeft(updatedLeft)
+    setTotal(left + updatedRight)
+  }
+
+
+
+  return (
+    <div>
+      {left}
+      <button onClick={handleLeftClick}>
+        left
+      </button>
+      <button onClick={handleRightClick}>
+        right
+      </button>
+      {right}
+      <p>{allClicks.join(' ')}</p>
+      <p>total {total}</p>
+    </div>
+  )
+}
+export default App
--- practica/React/React Docs/estado complejo/src/main.jsx
+++ practica/React/React Docs/estado complejo/src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client'
 
-import App from './App1'
+import App from './App2'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
