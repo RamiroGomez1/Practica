769a2c729017d8326fe51ae59ae01d4a76260c39
Ramiro Gomez <ramigomezbahia@gmail.com>
2024-08-26 15:49:57 -0300

    Ejercicio 1

---
+++ m3u4/ejercicio1/app.js
@@ -0,0 +1,11 @@
+const background = document.getElementById(bg)
+const body = document.body
+
+function changeColor(num) {
+    if(num === 1) body.style.backgroundColor = 'blue'
+    else if (num === 2) body.style.backgroundColor = 'black'
+    else body.style.backgroundColor = 'red'
+}   
+
+
+
