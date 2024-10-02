b84466c4dfae92f7fc9b6930dd98ee5fcdd37168
Ramiro Gomez <ramigomezbahia@gmail.com>
2024-10-02 12:51:46 -0300

    mensaje de error

---
+++ practica/React/React Docs/styless/src/App.jsx
@@ -0,0 +1,113 @@
+import { useState, useEffect } from 'react'
+import Note from './components/Note'
+import Notification from './components/Error'
+import noteService from './services/notes'
+import axios from 'axios'
+import './index.css'
+
+
+const App = (props) => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
+
+  useEffect(() => {
+    noteService
+    .getAll()
+    .then(initialNotes => {
+      setNotes(initialNotes)
+    })
+  }, [])
+
+  const addNote = (event) => {
+    event.preventDefault()
+
+    const noteObject = {
+      content: newNote,
+      important: Math.random() > 0.5,
+    }
+
+    axios
+    .post('http://localhost:3001/notes', noteObject)
+    .then(response => {
+      setNotes(notes.concat(response.data))
+      setNewNote('')
+    })
+
+    noteService
+    .create(noteObject)
+    .then(returnedNote => {
+      setNotes(notes.concat(returnedNote))
+      setNewNote('')
+    })
+  }
+
+  const handleNoteChange = (event) => {
+    event.preventDefault()
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
+
+
+
+
+  const toggleImportance = (id) => {
+    
+    const note = notes.find(n => n.id === id)
+    const changedNote = {...note, important: !note.important }
+    
+    noteService
+    .update(id, changedNote)
+    .then(returnedNote => {
+      setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+    })
+    .catch(error => {
+ 
+ 
+      //      alert(`the note '${note.content}' was already deleted from server`)
+      //En lugar del alert, pongo un serErrorMessage. Esto hace basicamente lo mismo pero desde un mensaje de la aplicacion, no una alerta del navegador
+    
+      setErrorMessage(
+      `Note ${note.content} was already removed from server`
+      )
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+ 
+      
+      setNotes(notes.filter(n => n.id !== id))
+    })
+  }
+
+
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <Notification message={errorMessage} />
+      <div>
+        <button onClick={()=> setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map((note, i) =>
+          <Note key={i}
+           note={note}
+           toggleImportance={() => toggleImportance(note.id)} />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type='submit'>save</button>
+      </form>
+    </div>
+  )
+}
+
+export default App 
---
+++ practica/React/React Docs/styless/src/services/notes.js
@@ -0,0 +1,27 @@
+//Este archivo es un modulo
+
+//Este modulo devuelve un objeto que tiene tres funciones (getAll, create, update)
+
+//Estas funciones se devuelven como propiedades que se ocupan de las notas 
+
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/notes'
+
+const getAll = () => {
+    const request = axios.get(baseUrl)
+    return request.then(response => response.data)
+}
+
+const create = newObject => {
+    const request = axios.post(baseUrl, newObject)
+    return request.then(response => response.data)
+}
+
+const update = (id, newObject) => {
+    const request = axios.put(`${baseUrl}/${id}`, newObject)
+    return request.then(response => response.data)
+}
+
+export default { getAll, create, update }
+
+// Hacemos mas compacto la exportacion del objeto
