20253b148486c62da977a9cdde92732ae506544e
Ramiro Gomez <ramigomezbahia@gmail.com>
2024-10-18 17:29:31 -0300

    m5u3

--- m5u3/app.js
+++ m5u3/app.js
@@ -25,11 +25,52 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-/*Ejemplo select */
+
+/*Ejemplo select 
 
 pool.query('select * from m5u2.empleados').then(function(resultados){
   console.log(resultados)
 })
+*/
+
+//insert
+
+/*
+
+var obj = {
+  id_emp: 23,
+  nombre:'Ramiro',
+  apellido:'Gomez',
+  trabajo:'Programador',
+  edad:19,
+  sueldo: 1500,
+  email: 'ramiro@mail.com'
+}
+
+pool.query('insert into m5u2.empleados set ?', [obj]).then(function(response){
+  console.log(resultados)
+})
+
+*/
+
+
+//update
+
+/*
+
+var id = 20;
+var obj = {
+  nombre: 'Pepe',
+  apellido: 'Martinez'
+}
+
+pool.query('update m5u2.empleados set ? where id_emp=?', [obj, id])
+
+*/
+
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
