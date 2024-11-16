cf188349da96b9ee696a94bf3771754b19e975f7
Ramiro Gomez <ramigomezbahia@gmail.com>
2024-11-15 21:24:00 -0300

    .

--- m5u4/BeFit/back/routes/admin/login.js
+++ m5u4/BeFit/back/routes/admin/login.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var usuariosModel = require('./../../models/usuariosModel');
+const { log } = require('util');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,5 +10,25 @@ router.get('/', function(req, res, next) {
     layout: 'admin/layout' // aca layout.hbs
   });
 });
+
+router.post('/', async(req, res, next) => {
+  try {
+    var usuario = req.body.usuario; // captura la informacion (username)
+    var password = req.body.password; // captura la contraseña
+
+    var data = await usuariosModel.getUserByUsernameAndPassword(usuario, password) //se envia la info guardada en los vars usuario y password
+
+    if(data != undefined){ //si tengo un registro
+        res.redirect('/admin/novedades') //redireccioname a la nueva pagina (/admin/novedades)
+    } else {
+      res.render('admin/login', {
+        layout:'admin/layout',
+        error: true
+      })
+    }
+  } catch (error) {
+    console.log(error);
+  }
+})
 
 module.exports = router;
