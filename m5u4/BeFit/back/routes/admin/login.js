4fe91d13d2bd26a1392ae20ddd34e5d11cd3a8b0
Ramiro Gomez <ramigomezbahia@gmail.com>
2024-11-17 21:47:20 -0300

    boton logout

--- m5u4/BeFit/back/routes/admin/login.js
+++ m5u4/BeFit/back/routes/admin/login.js
@@ -12,6 +12,13 @@ router.get('/', function (req, res, next) {
     });
 });
 
+router.get('/logout', function (req,res,next) {
+  req.session.destroy(); //destruir las variables de sesion (id, usuario)
+  res.render('admin/login', { //renderizame la pagina inicial de login
+    layout: 'admin/layout'
+  });
+})
+
 router.post('/', async (req, res, next) => {
   try {
     var user = req.body.user; // captura la informacion (username)
@@ -19,8 +26,10 @@ router.post('/', async (req, res, next) => {
     var data = await usuariosModel.getUserByUsernameAndPassword(user, password) //se envia la info guardada en los vars usuario y password
 
     if (data) { //si tengo un registro
+
       req.session.id_usuario = data.id;
       req.session.nombre = data.user;
+
       res.redirect('/admin/novedades') //redireccioname a la nueva pagina (/admin/novedades)
     } else {
       res.render('admin/login', {
--- m5u4/BeFit/back/routes/admin/novedades.js
+++ m5u4/BeFit/back/routes/admin/novedades.js
@@ -5,6 +5,7 @@ var router = express.Router();
 router.get('/', (req, res, next) => {
   res.render('admin/novedades', {
     layout: 'admin/layout',
+    persona: req.session.nombre
   });
 });
 
