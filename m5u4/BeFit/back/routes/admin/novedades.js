d15e7102525940a0c1da37a45dd4819e176f7de2
Ramiro Gomez <ramigomezbahia@gmail.com>
2024-11-18 21:50:04 -0300

    Agregar novedades dinamicamente

--- m5u4/BeFit/back/routes/admin/novedades.js
+++ m5u4/BeFit/back/routes/admin/novedades.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var novedadesModel = require('../../models/novedadesModel')
+var novedadesModel = require('../../models/novedadesModel');
+const { route } = require('./login');
 
 /* GET novedades page. */
 router.get('/', async function (req, res, next) {
@@ -12,5 +13,35 @@ router.get('/', async function (req, res, next) {
     novedades
   });
 });
+
+router.get('/agregar', (req, res, next) => {
+  res.render('admin/agregar', {
+    layout: 'admin/layout'
+  })
+})
+
+router.post('/agregar' //cuando el boton para agregar sea presionado, que se ejecute esta funcion
+  , async (req, res, next) => {
+  try {
+    if(req.body.titulo != '' && req.body.subtitulo != '' && req.body.cuerpo != '') // Si todos los campos de la nueva novedad estan completos, haz lo siguiente
+     {
+      await novedadesModel.insertNovedad(req.body);// mete la info de la novedad nueva en insertNovedad como objeto
+      res.redirect('/admin/novedades') //mandame a la pagina de novedades ya con mi novedad nueva puesta
+    } else {
+      res.render('admin/agregar', {
+        layout: 'admin/layout',
+        error: true,
+        message: 'Todos los campos son requeridos'
+      })
+    }
+  } catch (error) {
+    console.log(error)
+    res.render('admin/agregar', {
+      layout: 'admin/layout',
+      error: true,
+      message: 'No se cargo la novedad'
+    })
+  }
+})
 
 module.exports = router;
